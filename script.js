fec6d8b13d094a94a4b1c5e6441cb51fcba999fe
mahavishnu <mahanvishnu3@gamil.com>
2025-05-16 15:13:54 +0530

    newchanges

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
- $(function () {
-        $('[data-bs-toggle="popover"]').popover()
-    });
+// This is a simple OTP generator and verification script
+// It generates a 6-digit OTP and verifies it against user input
+    let otp = "";
+function myFunction(){
+    for (let i = 0; i<6; i++)
+    {
+        otp+=Math.floor(Math.random()*9);
+    }
+    document.getElementById('result').innerHTML = otp;
+} 
+function result()
+    {
+       const results =  document.getElementById('otp').value;
+       const result1 = document.getElementById('result1');
+       const result2 = document.getElementById('result2');
+        if(results == otp)
+        {
+            result1.innerHTML = "OTP is correct";
+            result1.style.color = "green";
+            result2.style.display = "block";
+            
+           
+        }
+        else
+        {
+            result1.innerHTML = "OTP is incorrect";
+            result1.style.color = "red";
+        }
+    }
