f2c3e88a2083d941b6a1c9f8c3348b1afc6a1858
mahavishnu <mahanvishnu3@gamil.com>
2025-05-16 16:28:39 +0530

    change

--- script.js
+++ script.js
@@ -6,8 +6,27 @@ function myFunction(){
     {
         otp+=Math.floor(Math.random()*9);
     }
-    document.getElementById('result').innerHTML = otp;
-} 
+    const otpvalue = document.getElementById('result').innerHTML = otp;
+    document.getElementById('otpvalid').style.display = "block";
+
+    var counter = 30;
+var value1 = document.getElementById('timer');
+var value2 = document.getElementById('time');
+var interval = setInterval(function() {
+    counter--;
+    // Display 'counter' wherever you want to display it.
+    if (counter <= 0) {
+     		clearInterval(interval);
+      	value1.innerHTML="<h3>please reclick otp button</h3>";  
+        return;
+    }else{
+    	value2.innerHTML =counter;
+      console.log( counter);
+    }
+}, 1000);
+    
+}
+
 function result()
     {
        const results =  document.getElementById('otp').value;
@@ -27,3 +46,21 @@ function result()
             result1.style.color = "red";
         }
     }
+    var count = 0
+    var time1 = document.getElementById('time1');
+    var time2 = document.getElementById('time2');
+    var interval1 =setInterval(function(){
+        count++;
+
+        if(count>60){
+            clearInterval(interval1);
+            time1.innerHTML="completed please stop"
+        }
+        else
+        {
+            time2.innerHTML = count;
+        }
+    },1000)
+
+
+
